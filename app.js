d1452345bb79ff427a22ab87c04914b7197a8112
Lokesh-Bongu <lokesh.bongu@tpsouthernodisha.com>
2024-03-29 18:27:21 +0530

    Initial Web

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 (function() {
     'use strict';
 
-    angular.module('RestaurantApp',['ngRoute'])
-    .config(RoutesConfig);
+    angular.module('RestaurantApp', ['ngRoute'])
+        .config(RoutesConfig)
+        .controller('MainController', MainController)
+        .controller('SignUpController', SignUpController)
+        .controller('MyInfoController', MyInfoController)
+        .service('SignUpService', SignUpService);
 
     RoutesConfig.$inject = ['$routeProvider'];
     function RoutesConfig($routeProvider) {
         $routeProvider
-        .when('/signup', {
-            templateUrl: 'signup.html',
-            controller: 'SignUpController as signupCtrl'
-        })
-        .when('/myinfo', {
-            templateUrl: 'myinfo.html',
-            controller: 'MyInfoController as myInfoCtrl'
-        })
-        .otherwise({
-            redirectTo: '/signup'
-        });
+            .when('/signup', {
+                templateUrl: 'signup.html',
+                controller: 'SignUpController as signupCtrl'
+            })
+            .when('/myinfo', {
+                templateUrl: 'myinfo.html',
+                controller: 'MyInfoController as myInfoCtrl'
+            })
+            .otherwise({
+                redirectTo: '/signup'
+            });
     }
-
-    angular.module('RestaurantApp')
-    .controller('MainController', MainController);
 
     MainController.$inject = ['$location'];
     function MainController($location) {
@@ -36,30 +37,32 @@
         };
     }
 
-    angular.module('RestaurantApp')
-    .controller('SignUpController', SignUpController);
-
-    SignUpController.$inject = ['SignUpService', '$location']; // Inject $location here
+    SignUpController.$inject = ['SignUpService', '$location'];
     function SignUpController(SignUpService, $location) {
-   
         var signupCtrl = this;
 
         signupCtrl.submitForm = function() {
-            // Form submission logic
-            SignUpService.saveUserData(signupCtrl.firstName, signupCtrl.lastName, signupCtrl.email, signupCtrl.phone, signupCtrl.favoriteMenuItem);
-            signupCtrl.message = "Your information has been saved.";
+            SignUpService.saveUserData(signupCtrl.firstName, signupCtrl.lastName, signupCtrl.email, signupCtrl.phone, signupCtrl.favoriteMenuItem)
+                .then(function() {
+                    signupCtrl.message = "Your information has been saved.";
+                })
+                .catch(function(error) {
+                    console.error("Error saving user data:", error);
+                    signupCtrl.message = "Failed to save user information.";
+                });
         };
 
         signupCtrl.checkMenuItem = function() {
             SignUpService.checkMenuItem(signupCtrl.favoriteMenuItem)
-            .then(function(response) {
-                signupCtrl.invalidMenuItem = response === null;
-            });
+                .then(function(response) {
+                    signupCtrl.invalidMenuItem = response === null;
+                })
+                .catch(function(error) {
+                    console.error("Error checking menu item:", error);
+                    signupCtrl.invalidMenuItem = true;
+                });
         };
     }
-
-    angular.module('RestaurantApp')
-    .controller('MyInfoController', MyInfoController);
 
     MyInfoController.$inject = ['SignUpService'];
     function MyInfoController(SignUpService) {
@@ -69,9 +72,6 @@
         myInfoCtrl.favoriteMenuItem = SignUpService.getFavoriteMenuItem();
     }
 
-    angular.module('RestaurantApp')
-    .service('SignUpService', SignUpService);
-
     SignUpService.$inject = ['$http'];
     function SignUpService($http) {
         var service = this;
@@ -79,7 +79,6 @@
         var favoriteMenuItem;
 
         service.saveUserData = function(firstName, lastName, email, phone, favoriteMenuItem) {
-            // Save user data
             userInfo = {
                 firstName: firstName,
                 lastName: lastName,
@@ -87,28 +86,30 @@
                 phone: phone
             };
             service.favoriteMenuItem = favoriteMenuItem;
+
+            // Simulate asynchronous behavior with a promise
+            return Promise.resolve(); // Replace this with actual saving logic if needed
         };
 
         service.checkMenuItem = function(menuItem) {
             return $http.get('https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json')
-            .then(function(response) {
-                var menuItems = response.data;
-                console.log("menuItems",menuItems)
-                var menuItemExists = false;
-                for (var categoryKey in menuItems) {
-                    var category = menuItems[categoryKey];
-                    for (var i = 0; i < category.menu_items.length; i++) {
-                        if (category.menu_items[i].name === menuItem) {
-                            menuItemExists = true;
+                .then(function(response) {
+                    var menuItems = response.data;
+                    var menuItemExists = false;
+                    for (var categoryKey in menuItems) {
+                        var category = menuItems[categoryKey];
+                        for (var i = 0; i < category.menu_items.length; i++) {
+                            if (category.menu_items[i].name === menuItem) {
+                                menuItemExists = true;
+                                break;
+                            }
+                        }
+                        if (menuItemExists) {
                             break;
                         }
                     }
-                    if (menuItemExists) {
-                        break;
-                    }
-                }
-                return menuItemExists ? menuItem : null;
-            });
+                    return menuItemExists ? menuItem : null;
+                });
         };
 
         service.getUserInfo = function() {
@@ -116,7 +117,7 @@
         };
 
         service.getFavoriteMenuItem = function() {
-            console.log("lllll",favoriteMenuItem)
+            console.log("kkkkkkkk",favoriteMenuItem)
             return favoriteMenuItem;
         };
     }
