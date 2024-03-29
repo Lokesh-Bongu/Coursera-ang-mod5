45e5c136744bca5dbc0f90c205db77f3d2396af7
Lokesh-Bongu <lokesh.bongu@tpsouthernodisha.com>
2024-03-29 13:08:04 +0530

    Initial Web

--- app.js
+++ app.js
@@ -16,7 +16,7 @@
             controller: 'MyInfoController as myInfoCtrl'
         })
         .otherwise({
-            redirectTo: '/'
+            redirectTo: '/signup'
         });
     }
 
@@ -39,12 +39,21 @@
         };
     }
 
-    SignUpController.$inject = ['SignUpService'];
-    function SignUpController(SignUpService) {
+    SignUpController.$inject = ['SignUpService', '$location'];
+    function SignUpController(SignUpService, $location) {
         var signupCtrl = this;
 
         signupCtrl.submitForm = function() {
-            // Submit form logic here
+            // Form submission logic
+            SignUpService.saveUserData(signupCtrl.firstName, signupCtrl.lastName, signupCtrl.email, signupCtrl.phone, signupCtrl.favoriteMenuItem);
+            signupCtrl.message = "Your information has been saved.";
+        };
+
+        signupCtrl.checkMenuItem = function() {
+            SignUpService.checkMenuItem(signupCtrl.favoriteMenuItem)
+            .then(function(response) {
+                signupCtrl.invalidMenuItem = response === null;
+            });
         };
     }
 
@@ -56,9 +65,50 @@
         myInfoCtrl.favoriteMenuItem = SignUpService.getFavoriteMenuItem();
     }
 
-    function SignUpService() {
+    SignUpService.$inject = ['$http'];
+    function SignUpService($http) {
         var service = this;
+        var userInfo;
+        var favoriteMenuItem;
 
-        // Implement methods to save and retrieve user information and favorite menu item
+        service.saveUserData = function(firstName, lastName, email, phone, favoriteMenuItem) {
+            // Save user data
+            userInfo = {
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                phone: phone
+            };
+            service.favoriteMenuItem = favoriteMenuItem;
+        };
+
+        service.checkMenuItem = function(menuItem) {
+            return $http.get('https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json')
+            .then(function(response) {
+                var menuItems = response.data;
+                var menuItemExists = false;
+                for (var categoryKey in menuItems) {
+                    var category = menuItems[categoryKey];
+                    for (var i = 0; i < category.menu_items.length; i++) {
+                        if (category.menu_items[i].name === menuItem) {
+                            menuItemExists = true;
+                            break;
+                        }
+                    }
+                    if (menuItemExists) {
+                        break;
+                    }
+                }
+                return menuItemExists ? menuItem : null;
+            });
+        };
+
+        service.getUserInfo = function() {
+            return userInfo;
+        };
+
+        service.getFavoriteMenuItem = function() {
+            return favoriteMenuItem;
+        };
     }
 })();
